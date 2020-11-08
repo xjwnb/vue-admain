48a9e9a42169f41aafa9e421ba88a2ab89d730ac
1565163445@qq.com <1565163445@qq.com>
2020-11-08 16:51:31 +0800

    update Color 颜色数据

--- src/mock/color/index.js
+++ src/mock/color/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-11-08 16:16:44
- * @LastEditTime: 2020-11-08 16:49:58
+ * @LastEditTime: 2020-11-08 16:51:01
  * @LastEditors: Please set LastEditors
  * @Description: 导出颜色列表
  * @FilePath: \vue-admain\src\mock\color\index.js
@@ -9,166 +9,166 @@
 export const colorList = {
   Aussie_Palette: [
     {
-      id: 1-1,
+      id: 1 - 1,
       color: "#1abc9c"
     },
     {
-      id: 1-2,
+      id: 1 - 2,
       color: "#2ecc71"
     },
     {
-      id: 1-3,
+      id: 1 - 3,
       color: "#3498db"
     },
     {
-      id: 1-4,
+      id: 1 - 4,
       color: "#9b59b6"
     },
     {
-      id: 1-5,
+      id: 1 - 5,
       color: "#34495e"
     },
     {
-      id: 1-6,
+      id: 1 - 6,
       color: "#16a085"
     },
     {
-      id: 1-7,
+      id: 1 - 7,
       color: "#27ae60"
     },
     {
-      id: 1-8,
+      id: 1 - 8,
       color: "#2980b9"
     },
     {
-      id: 1-9,
+      id: 1 - 9,
       color: "#8e44ad"
     },
     {
-      id: 1-10,
+      id: 1 - 10,
       color: "#2c3e50"
     },
     {
-      id: 1-11,
+      id: 1 - 11,
       color: "#f1c40f"
     },
     {
-      id: 1-12,
+      id: 1 - 12,
       color: "#e67e22"
     },
     {
-      id: 1-13,
+      id: 1 - 13,
       color: "#e74c3c"
     },
     {
-      id: 1-14,
+      id: 1 - 14,
       color: "#ecf0f1"
     },
     {
-      id: 1-15,
+      id: 1 - 15,
       color: "#95a5a6"
     },
     {
-      id: 1-16,
+      id: 1 - 16,
       color: "#f39c12"
     },
     {
-      id: 1-17,
+      id: 1 - 17,
       color: "#d35400"
     },
     {
-      id: 1-18,
+      id: 1 - 18,
       color: "#c0392b"
     },
     {
-      id: 1-19,
+      id: 1 - 19,
       color: "#bdc3c7"
     },
     {
-      id: 1-20,
+      id: 1 - 20,
       color: "#7f8c8d"
-    },
+    }
   ],
   British_Palette: [
     {
-      id: 1-1,
+      id: 1 - 1,
       color: "#00a8ff"
     },
     {
-      id: 1-2,
+      id: 1 - 2,
       color: "#9c88ff"
     },
     {
-      id: 1-3,
+      id: 1 - 3,
       color: "#fbc531"
     },
     {
-      id: 1-4,
+      id: 1 - 4,
       color: "#4cd137"
     },
     {
-      id: 1-5,
+      id: 1 - 5,
       color: "#487eb0"
     },
     {
-      id: 1-6,
+      id: 1 - 6,
       color: "#0097e6"
     },
     {
-      id: 1-7,
+      id: 1 - 7,
       color: "#8c7ae6"
     },
     {
-      id: 1-8,
+      id: 1 - 8,
       color: "#e1b12c"
     },
     {
-      id: 1-9,
+      id: 1 - 9,
       color: "#44bd32"
     },
     {
-      id: 1-10,
+      id: 1 - 10,
       color: "#40739e"
     },
     {
-      id: 1-11,
+      id: 1 - 11,
       color: "#e84118"
     },
     {
-      id: 1-12,
+      id: 1 - 12,
       color: "#f5f6fa"
     },
     {
-      id: 1-13,
+      id: 1 - 13,
       color: "#7f8fa6"
     },
     {
-      id: 1-14,
+      id: 1 - 14,
       color: "#273c75"
     },
     {
-      id: 1-15,
+      id: 1 - 15,
       color: "#353b48"
     },
     {
-      id: 1-16,
+      id: 1 - 16,
       color: "#c23616"
     },
     {
-      id: 1-17,
+      id: 1 - 17,
       color: "#dcdde1"
     },
     {
-      id: 1-18,
+      id: 1 - 18,
       color: "#718093"
     },
     {
-      id: 1-19,
+      id: 1 - 19,
       color: "#192a56"
     },
     {
-      id: 1-20,
+      id: 1 - 20,
       color: "#2f3640"
-    },
+    }
   ]
-}
+};
