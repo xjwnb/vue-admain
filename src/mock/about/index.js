a5dbe0a3c229f4379f7f28c7a59a9fb8909e016e
1565163445@qq.com <1565163445@qq.com>
2020-11-09 17:59:34 +0800

    update 关于页面

---
+++ src/mock/about/index.js
@@ -0,0 +1,34 @@
+/*
+ * @Author: your name
+ * @Date: 2020-11-09 16:30:36
+ * @LastEditTime: 2020-11-09 16:37:00
+ * @LastEditors: Please set LastEditors
+ * @Description: 关于数据
+ * @FilePath: \vue-admain\src\mock\about\index.js
+ */
+export const aboutMsg = {
+  projectName: "Vue-Admin",
+  developer: "小卡车",
+  technology: [
+    {
+      id: 1,
+      name: "Vue",
+      src: "https://cn.vuejs.org/"
+    },
+    {
+      id: 2,
+      name: "Vue-router",
+      src: "https://router.vuejs.org/zh/"
+    },
+    {
+      id: 3,
+      name: "Vuex",
+      src: "https://vuex.vuejs.org/zh/"
+    },
+    {
+      id: 4,
+      name: "Mockjs",
+      src: "http://mockjs.com/"
+    }
+  ]
+}
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,72 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-11-03 18:13:40
+ * @LastEditTime: 2020-11-09 17:57:04
+ * @LastEditors: Please set LastEditors
+ * @Description: 关于页面
+ * @FilePath: \vue-admain\src\views\About.vue
+-->
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>{{ msg.projectName }}</h1>
+    <h4>开发者：{{ msg.developer }}</h4>
+    <div class="technology" @click="clickHamdler">
+      <template v-for="item in msg.technology" v-key="item.id">
+        <div class="tech" :id="item.src">
+          {{ item.name }}
+        </div>
+      </template>
+    </div>
   </div>
 </template>
+
+<script>
+import { aboutMsg } from "@/mock/about";
+
+export default {
+  name: "About",
+  data() {
+    return {
+      msg: aboutMsg,
+    };
+  },
+  created() {},
+  methods: {
+    clickHamdler(e) {
+      let url = e.target.id;
+      // window.location.href = src;
+      window.open(url, "_blank");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.about {
+  text-align: center;
+}
+h1 {
+  text-shadow: 1px 2px 2px aquamarine;
+  font-size: 2rem;
+}
+h4 {
+  margin-top: 1rem;
+}
+.about {
+  width: 100%;
+  min-width: 500px;
+}
+.technology {
+  margin-top: 2rem;
+  display: flex;
+  justify-content: space-around;
+  background-color: ＃1abc9c;
+}
+.tech {
+  cursor: pointer;
+  width: 20%;
+  height: 100px;
+  line-height: 100px;
+  background-color: aquamarine;
+}
+</style>
