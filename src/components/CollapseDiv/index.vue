ae4464807ebc87d190689dbf751012f909141c29
1565163445@qq.com <1565163445@qq.com>
2020-11-10 16:49:00 +0800

    update collapse

--- src/components/CollapseDiv/index.vue
+++ src/components/CollapseDiv/index.vue
@@ -1,15 +1,18 @@
 <!--
  * @Author: your name
  * @Date: 2020-11-10 12:41:43
- * @LastEditTime: 2020-11-10 15:59:14
+ * @LastEditTime: 2020-11-10 16:44:19
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \vue-admain\src\components\CollapseDiv\index.vue
 -->
 <template>
-  <el-collapse-item :title="collapseData.title" :name="collapseData.id">
+  <el-collapse-item
+    :title="`标题${collapseData.id}：` + collapseData.title"
+    :name="collapseData.id"
+  >
     <div>
-      {{ collapseData.body }}
+      {{" 内容：" + collapseData.body }}
     </div>
   </el-collapse-item>
 </template>
